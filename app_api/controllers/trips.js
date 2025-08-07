c8f6c8ee15131765838b223b78a49d08e503ea30
Chris-Merced <christopher.r.merced@gmail.com>
2025-08-07 11:53:50 -0500

    Working Module 6 changes

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -2,6 +2,68 @@ const mongoose = require('mongoose')
 const Trip = require('../models/travlr')
 const Model = mongoose.model('trips')
 
+
+// PUT: /trips/:tripCode - Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsUpdateTrip = async(req, res) => {
+// Uncomment for debugging
+console.log(req.params);
+console.log(req.body);
+const q = await Model
+.findOneAndUpdate(
+{ 'code' : req.params.tripCode },
+{
+code: req.body.code,
+name: req.body.name,
+length: req.body.length,
+start: req.body.start,
+resort: req.body.resort,
+perPerson: req.body.perPerson,
+image: req.body.image,
+description: req.body.description
+}
+)
+.exec();
+if(!q)
+{ // Database returned no data
+return res
+.status(400)
+.json(err);
+} else { // Return resulting updated trip
+return res
+.status(201)
+.json(q);
+}
+// Uncomment the following line to show results of
+operation
+// on the console
+// console.log(q);
+};
+
+const tripsAddTrip = async (req, res)=>{
+
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description,
+    })
+
+    const q = await newTrip.save();
+
+        if(!q){
+            return res.status(400).json("WHAT THE FUCK IS GOING ON " + err.message)
+        }else{
+            return res.status(201).json(q)
+        }
+    console.log(q)
+};
+
 const tripsList = async(req, res) => {
     const q = await Model.find({}).exec();
     console.log(q)
@@ -25,5 +87,7 @@ const tripsFindByCode = async(req, res) => {
 
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 }
